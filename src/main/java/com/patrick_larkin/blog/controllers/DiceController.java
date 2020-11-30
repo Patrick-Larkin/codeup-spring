e06ef9805952cf96c1acb277d3af0df93fffb731
Patrick Larkin <patricklarkin995@gmail.com>
2020-11-30 15:16:52 -0500

    finished dice game exercise

--- src/main/java/com/patrick_larkin/blog/controllers/DiceController.java
+++ src/main/java/com/patrick_larkin/blog/controllers/DiceController.java
@@ -2,20 +2,32 @@ package com.patrick_larkin.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class DiceController {
+
+	@GetMapping("/roll-dice/{guess}")
+	public String rollDice(@PathVariable int guess, Model model) {
+		int dice = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+		model.addAttribute("guess", guess);
+		model.addAttribute("dice", dice);
+		model.addAttribute("isCorrectGuess", guess == dice);
+		return "roll-dice";
+	}
+
 	@GetMapping("/roll-dice")
+	public String showDiceGame() {
 		return "/dicegame";
 	}
 
-	@GetMapping("/roll-dice/n")
-	public String showJoinForm(Model model) {
-
-		return "join";
+	@PostMapping("/roll-dice")
+	public String playDiceGame(@RequestParam(name = "roll") String roll, Model model) {
+		model.addAttribute("roll", roll + "!");
+		return "/dicegame";
 	}
 }
